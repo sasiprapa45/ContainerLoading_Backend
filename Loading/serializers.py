5f8ae25a2e2cd49a84f3884bfe1686f406739948
Sasiprapa45 <1yayee1.yy@gmail.com>
2024-05-05 21:43:44 +0700

    api insertdata

--- Loading/serializers.py
+++ Loading/serializers.py
@@ -1,10 +1,31 @@
 from rest_framework import serializers
 from .models import Cargoes, Container, TypeCargo,TypeContainer, Position,Project
-class CargoesSerializer(serializers.HyperlinkedModelSerializer):
+
+
+
+# class CargoesChildSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Cargoes
+#         fields = ['name', 'type_cargo', 'weight', 'project_id']
+
+class CargoesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cargoes
-        fields =  '__all__'
+        fields = ['name', 'type_cargo', 'weight', 'project_id']
+
+# class CargoesSerializer(serializers.ListSerializer):
+#     child = CargoesChildSerializer()
+
+#     def create(self, validated_data):
+#         cargoes = [Cargoes(**item) for item in validated_data]
+#         return Cargoes.objects.bulk_create(cargoes)
+
+#     class Meta:
+#         model = Cargoes
+#         fields = ['name', 'type_cargo', 'weight', 'project_id']
         
+
+
 class ContainerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Container
